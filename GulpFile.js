412c7586554fe76d75ceb59ee79c4f5b6851ae65
Andrew Petersen <andrew.petersen15@gmail.com>
2014-02-26 00:58:59 -0600

    added gulp and proved search works

--- GulpFile.js
+++ GulpFile.js
@@ -1,15 +1,20 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
 var concat = require('gulp-concat');
 
-
-gulp.task('restDao', function() {
-	gulp.src(['./baseDao.js', './restDao.js'])
-		.pipe(concat("SPScript.RestDao.js"))
-		.pipe(gulp.dest('./dist/'));
+gulp.task('search', function() {
+  gulp.src(['./baseDao.js', './restDao.js', 'querystring.js', 'search.js'])
+    .pipe(concat("SPScript.Search.js"))
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('search', function() {
-	gulp.src(['./baseDao.js', './restDao.js', 'queryString.js', 'search.js'])
-		.pipe(concat("SPScript.Search.js"))
-		.pipe(gulp.dest('./dist/'));
+gulp.task('rest', function() {
+  gulp.src(['./baseDao.js', './restDao.js'])
+    .pipe(concat("SPScript.RestDao.js"))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('crossdomain', function() {
+  gulp.src(['./baseDao.js', './odatahelpers.js', './crossDomainDao.js'])
+    .pipe(concat("SPScript.CrossDomain.js"))
+    .pipe(gulp.dest('./dist/'));
 });
