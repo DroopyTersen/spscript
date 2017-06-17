2c457b3828175bd893024682420bc73d5ce9929d
Andrew Petersen <Andrew Petersen>
2017-06-17 11:31:20 -0500

    cleaned npm scripts (espcially test:browser) to support ngrok subdomains.

--- src/tests/run.js
+++
@@ -1,5 +0,0 @@
-var engine = require("spbrander");
-var siteUrl = "https://andrewpetersen.sharepoint.com/spscript";
-
-engine.page(siteUrl, "/SitePages/Tests.aspx")
-        .debug("./src/tests/test.html", "tests");
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,11 @@ module.exports = {
     entry: {
         "spscript": "./entries/spscript.js",
         "tests": "./entries/tests.js",
-        // "plugins": "./entries/plugins.js"
     },
     output: {
         path: __dirname + "/dist/v2",
         filename: "[name].js",
         sourceMapFilename: "[name].js.map"
     },
-    // devtool: "source-map",
-    externals: {
-        "jquery": "jQuery" //assume jquery is added through cdn
-    }
+    devtool: "source-map",
 };
