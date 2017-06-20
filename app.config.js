c92e3d2ac91cc351684010062814926b5e281e89
Andrew Petersen <Andrew Petersen>
2017-06-19 19:36:46 -0500

    removed config

--- app.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    spLanguage: "en-us",
-    clientKey: "bb0fade0-5336-40a1-857c-2a9f2946da83",
-    clientSecret: "ehmNv5jEnHM5U9FaHEVuWBrRhpZg9ooOC+KELZRMYKU=",
-    spSiteUrl: "https://andrewpetersen.sharepoint.com/spscript"
-};
