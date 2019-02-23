76625ad618deae9d78167cb57f1722fa3e0604ce
Andrew Petersen <andrew.petersen15@gmail.com>
2019-02-22 22:39:27 -0600

    setup the umd build

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,18 @@
-import utils from "./utils";
+import utilsImport from "./utils";
 import { Utils } from "./utils/IUtils";
 import Context from "./context/Context";
-import CSR, { CSRUtils } from "./csr";
 import { ContextOptions } from "./context/interfaces";
 declare var global: any;
 
-export interface SPScript {
-  /** Utility functions*/
-  utils: Utils;
-  /** Creates an SPScript data context. If no url is passed, it uses current web. */
-  createContext(url?: string, options?: ContextOptions): Context;
-  /** Helper functions for creating REST Api HTTP headers. */
-  CSR: CSRUtils;
-}
-
-var spscript: SPScript = {
-  utils,
-  CSR,
-  createContext(url?: string, options?: ContextOptions) {
-    try {
-      // TODO: use get Site url util
-      if (!url && global._spPageContextInfo) {
-        url = global._spPageContextInfo.webAbsoluteUrl;
-      }
-      return new Context(url, options);
-    } catch (ex) {
-      throw new Error("Unable to create SPScript Context: " + ex.message);
+export const utils = utilsImport;
+export function createContext(url?: string, options?: ContextOptions) {
+  try {
+    // TODO: use get Site url util
+    if (!url && global._spPageContextInfo) {
+      url = global._spPageContextInfo.webAbsoluteUrl;
     }
+    return new Context(url, options);
+  } catch (ex) {
+    throw new Error("Unable to create SPScript Context: " + ex.message);
   }
-};
-
-export default spscript;
+}
