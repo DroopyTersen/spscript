f570b7f930f027341cbc73a64bad8d516ee20afe
Petersen <apetersen@skylinetechnologies.com>
2019-03-18 18:07:51 -0500

    enabled the copyfile test now that write operations work

--- __tests__/namespace.test.ts
+++ __tests__/namespace.test.ts
@@ -1,12 +1,12 @@
-import * as SPScript from "../src/index";
+import SPScript from "../src/index";
 require("dotenv").config();
 
 describe("SPScript Namespace", () => {
-  test("Should have a 'SPScript.createContext()' method", function() {
-    expect(SPScript).toHaveProperty("createContext");
-    expect(typeof SPScript.createContext).toBe("function");
-  });
-  test("Should have a 'SPScript.utils' namespace", function() {
-    expect(SPScript).toHaveProperty("utils");
-  });
+	test("Should have a 'SPScript.createContext()' method", function() {
+		expect(SPScript).toHaveProperty("createContext");
+		expect(typeof SPScript.createContext).toBe("function");
+	});
+	test("Should have a 'SPScript.utils' namespace", function() {
+		expect(SPScript).toHaveProperty("utils");
+	});
 });
